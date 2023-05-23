4125d9d734833c6303ab55cb3ccfef046a4ebcac
QuincyClain <pesotskiyvlad9@gmail.com>
2023-05-24 02:27:44 +0300

    finished second time ))

--- итиродЛР345/LR3/Internet-technology-main/LR4-5/scripts/film-pick.js
+++
@@ -1,5 +0,0 @@
-function setData(name, time) {
-    window.localStorage.setItem("time", time);
-    window.localStorage.setItem("film", name);
-    location.href='../choosePage/choose.html';
-}
--- итиродЛР345/LR3/Internet-technology-main/LR4-5/scripts/logout.js
+++
@@ -1,4 +0,0 @@
-function showLogoutButton() {
-    document.getElementById("login").classList.remove("invisible");
-    document.getElementById("reg").classList.remove("invisible");
-}
